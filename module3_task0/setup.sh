83a4f2b91e5e2a6b7ea373df643f0b2ff54cbffd
Noémie Huynh <huynhtieuquy@gmail.com>
2023-01-06 20:45:38 +0100

    update task setup3

--- module3_task0/setup.sh
+++ module3_task0/setup.sh
@@ -1,15 +1,5 @@
-# Update and install 'Hugo' and 'Make'
 apt-get update && apt-get install -y hugo make
-
-# Install 'curl'
-apt-get install curl -y
-
-# Download lastest release of 'Hugo'
-curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-
-# Install the latest version of 'Hugo'
-apt-get install ./install_hugo.deb
-
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt install ./hugo.deb
 rm hugo.deb
-# Build the website
 make build
